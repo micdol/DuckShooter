a67287f9a9148bd5d32ba86ad5d44644f6ae08d5
michalinaeee <michalinawysocka@gmail.com>
2017-10-26 00:41:58 +0200

    I think thats it, finito.

---
+++ DuckShooter/Duck.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DuckShooter
+{
+    class Duck : Image
+    {
+        const int WIDTH = 80;
+        const int HEIGHT = 80;
+
+        private double amp;
+        private double phase;
+        private double period;
+        private double velocity;
+        private double maxX;
+
+        //top left pos
+        private Point pos;
+        public Point Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+
+        private bool isAlive;
+        public bool IsAlive
+        {
+            get { return isAlive; }
+        }
+
+        public Duck(Point pos, double maxX) : base()
+        {
+            this.pos = pos; this.maxX = maxX; isAlive = true;
+            Width = WIDTH;
+            Height = HEIGHT;
+            VerticalAlignment = VerticalAlignment.Top;
+            HorizontalAlignment = HorizontalAlignment.Left;
+            Source = new BitmapImage(new Uri("Resources/Images/duck.png", UriKind.Relative));
+            Margin = new Thickness(pos.X, pos.Y, 0, 0);
+
+            var r = new Random();
+            amp = r.Next(20, 50);
+            phase = r.NextDouble() * Math.PI * 2.0;
+            period = r.Next(100, 500);
+            velocity = r.NextDouble() * 3 + 1;
+        }
+
+        public void move()
+        {
+            var nx = (pos.X - velocity);
+            var ny = pos.Y + (amp * Math.Sin((Math.PI * 2.0 * nx + phase) / period));
+            Margin = new Thickness(nx, ny, 0, 0);
+            pos = new Point(nx, pos.Y);
+            if(nx < -WIDTH)
+            {
+                velocity *= -1;
+                RenderTransformOrigin = new Point(0.5, 0.5);
+                ScaleTransform flipTrans = new ScaleTransform();
+                flipTrans.ScaleX = -1; 
+                RenderTransform = flipTrans;
+            }
+            else if(nx > maxX)
+            {
+                velocity *= -1;
+                RenderTransformOrigin = new Point(0.5, 0.5);
+                ScaleTransform flipTrans = new ScaleTransform();
+                flipTrans.ScaleX = 1;
+                RenderTransform = flipTrans;
+            }
+        }
+
+        public bool shoot(Point shot)
+        {
+            Rect r = new Rect(pos, new Size(WIDTH, HEIGHT));
+            return r.Contains(shot);
+        }
+        
+        
+    }
+}
--- DuckShooter/MainWindow.xaml.cs
+++ DuckShooter/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DuckShooter
 {
@@ -20,9 +10,64 @@ namespace DuckShooter
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Duck> duckList;
+        private int numOfDuckShot;
+        private const int MAX_NUM_OF_DUCKS = 5;
+
         public MainWindow()
         {
             InitializeComponent();
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick; //event!!
+            dispatcherTimer.Interval = new TimeSpan(1000 / 24, 0, 0);
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+            dispatcherTimer.Start();
+            duckList = new List<Duck>();
+            numOfDuckShot = 0;
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (duckList.Count < MAX_NUM_OF_DUCKS)
+            {
+                Duck d = new Duck(
+                    new Point(
+                        window_DuckShooter.Width,
+                        new Random().Next(25, (int)window_DuckShooter.Height - 100)),
+                    window_DuckShooter.Width);
+                duckList.Add(d);
+                grid_windowDuckShooter.Children.Add(d);
+            }
+
+            foreach(Duck d in duckList)
+            {
+                d.move();
+            }
+        }
+
+        private void window_DuckShooter_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point pos = e.GetPosition(window_DuckShooter);
+            var w = image_target.Width;
+            var h = image_target.Height;
+            image_target.Margin = new Thickness(pos.X - w / 2.0, pos.Y - h / 2.0, 0, 0);
+        }
+
+        private void image_target_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            for (int i = 0; i < duckList.Count; i++)
+            {
+                Duck d = duckList[i];
+                if (d.shoot(e.GetPosition(window_DuckShooter))) 
+                {
+                    Console.WriteLine("Removing duck...");
+                    grid_windowDuckShooter.Children.Remove(d);
+                    duckList.RemoveRange(i, 1);
+                    numOfDuckShot++;
+                    break;
+                }
+            }
+            label_duckCounter.Content = "DuckCounter: " + numOfDuckShot;
         }
     }
 }
